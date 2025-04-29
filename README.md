347beacdc95ba7e72cd28aa439aad2fb0169b155
SilentWhisky <pistolarn@gmail.com>
2025-04-30 00:30:39 +0300

    Made a screen + drawn the player

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 ~ This is a Phython project, building a small game to practice managing different python files
 and practicing OOT.
 
+~ made a BLACK screen window, and Drawn the player [a Trinagle with a CircleShape HITBOX]
--- main.py
+++ main.py
@@ -3,22 +3,22 @@
 # throughout this file
 import pygame
 from constants import*
+from player import*
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    fps = pygame.time.Clock()
+    dt = 0
+    gamer = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(color="black")
+        gamer.draw(screen)
         pygame.display.flip()
-
-        #print("Starting Asteroids!")
-        #print(f"Screen width: {SCREEN_WIDTH}")
-        #print(f"Screen height: {SCREEN_HEIGHT}")
-
-
+        dt = fps.tick(60) / 1000
 
 if __name__ == "__main__":
     main()  
